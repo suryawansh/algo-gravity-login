8e9a6c2a81e222cad47c8d53d7e9db8e20301d11
@Kavita.suryawanshi <suryawanshi17298@gmail.com>
2024-03-17 02:09:10 +0530

    login page code added

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import PhoneNumber from "./components/phone-number/PhoneNumber";
+import PhoneNumberContinue from "./components/phone-number-continue/PhoneNumberContinue";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <PhoneNumber /> */}
+      <PhoneNumberContinue />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/phone-number-continue/PhoneNumberContinue.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import "./phone-number-continue.css";
+
+const PhoneNumberContinue = () => {
+  const [MobileNumber, setMobileNumber] = useState("");
+
+  return (
+    <div className="login-root">
+      <div className="netnimbus-logo-root">
+        <div className="netnimbus-logo">
+          <img src={require("../../assets/Net Nimbus Logo Final-01 1.png")} />
+        </div>
+        <div className="netnimbus-details">
+          <p className="netnimbus-details-p">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </p>
+          <p className="netnimbus-details-p">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </p>
+          <p className="netnimbus-details-p">
+            Lorem ipsum dolor sit amet,consectetur adipiscing elit.{" "}
+          </p>
+          <p className="netnimbus-details-p">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </p>
+        </div>
+      </div>
+
+      <div className="phone-number-root">
+        <div>
+          <h1 className="login-text">Login</h1>
+        </div>
+        <div className="phone-number">
+          <div className="phone-number-text">Phone Number</div>
+          <input
+            name=""
+            placeholder="+91"
+            onChange={(e) => setMobileNumber(e.target.value)}
+            className="phone-number-input"
+          />
+        </div>
+        <div className="get-otp-root">
+          <button type="default" className="number-continue">
+            Continue
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PhoneNumberContinue;
